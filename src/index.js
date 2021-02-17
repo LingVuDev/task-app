54081e31c846f3b0bba72ca7bfe8f154711f9d43
Ling Vu <manh-ling.vu@retresco.de>
2021-02-17 08:46:29 +0100

    Create and style a list of data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
+import initialData from './initial-data';
+import Column from './Column';
 
-const App = () => 'Hello World';
+class App extends React.Component {
+  state = initialData;
+
+  render() {
+    return this.state.columnOrder.map(columnId => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+      return <Column key={column.id} column={column} tasks={tasks} />;
+    })
+  }
+
+}
 
 ReactDOM.render(
   <React.StrictMode>
