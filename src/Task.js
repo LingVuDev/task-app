0a848e9d0c9f92083776d71bd2105e8f1aee3a92
Ling Vu <manh-ling.vu@retresco.de>
2021-02-17 09:18:36 +0100

    Reorder the List with react-beautiful-dnd

--- src/Task.js
+++ src/Task.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react';
 import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
 
 const Container = styled.div`
 	border: 1px solid lightgrey;
 	border-radius: 2px;
 	padding: 8px;
 	margin-bottom: 8px;
+	background-color: white;
 `;
 
-export class Task extends Component {
-  render() {
-    return (
-      <Container>{this.props.task.content}</Container>
-    )
-  }
+export default class Task extends React.Component {
+	render() {
+		return (
+			<Draggable draggableId={this.props.task.id} index={this.props.index}>
+				{(provided) => (
+					<Container {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+						{this.props.task.content}
+					</Container>
+				)}
+			</Draggable>
+		);
+	}
 }
 
-export default Task
--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
+import './index.css';
 import initialData from './initial-data';
 import Column from './Column';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 class App extends React.Component {
-  state = initialData;
+	state = initialData;
 
-  render() {
-    return this.state.columnOrder.map(columnId => {
-      const column = this.state.columns[columnId];
-      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+	onDragEnd = (result) => {
+		// TODO: reorder our column
+	};
 
-      return <Column key={column.id} column={column} tasks={tasks} />;
-    })
-  }
+	render() {
+		return (
+			<DragDropContext onDragEnd={this.onDragEnd}>
+				{this.state.columnOrder.map((columnId) => {
+					const column = this.state.columns[columnId];
+					const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
 
+					return <Column key={column.id} column={column} tasks={tasks} />;
+				})}
+			</DragDropContext>
+		);
+	}
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
